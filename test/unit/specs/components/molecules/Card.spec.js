ff120f3bff621e9568fb293b6085ce7d060cdd6b
ayuzaka <ayuzaka.f@gmail.com>
2019-10-20 12:32:09 +0900

    fix ut

--- test/unit/specs/components/molecules/Card.spec.js
+++ test/unit/specs/components/molecules/Card.spec.js
@@ -5,20 +5,13 @@ describe('Card.vue', () => {
   describe('プロパティ', () => {
     describe('デフォルト', () => {
       test('propsで渡したデータがカードに表示されていること', () => {
-        const task = {
-          id: 0,
-          name: 'sample',
-          describe: '',
-          status_id: 0,
-          position: 0,
-          isArchive: false,
-        };
+        const taskTitle = 'sample';
         const wrapper = shallowMount(Card, {
-          propsData: { task },
+          propsData: { title: taskTitle },
         });
         const title = wrapper.find('.card-title');
 
-        expect(title.text()).toBe(task.name);
+        expect(title.text()).toBe(taskTitle);
       });
     });
   });
